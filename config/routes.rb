0f2c3f0ae64429abbae6578f6883ff4a8316b22c
DennisDaddy <softteez95@gmail.com>
2018-09-25 20:27:07 +0300

    Generate posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :posts
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
